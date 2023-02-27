6fdaf2cb182ecc1df81480ecd98947088f742744
Aldoshin_Alexandr <aldoshin_a@internet.ru>
2023-02-27 15:18:02 +0300

    В программе организовано считывание данных, введённых ч/з пробел и преобразование в массив, перебор и вывод на экран массива + счётчик кол-ва ячеек для нового массива удовлетворяющего условию

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter array strings --> ");
+string Arr = Convert.ToString(Console.ReadLine());
+string[] Array = Arr.Split(' ');
+int count = 0;
+void LengArr(string[] Array)
+{
+    for (int i = 0; i < Array.GetLength(0) - 1; i++)
+    {
+        Console.Write($"{Array[i]} ");
+        if (Array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count);
+}
+LengArr(Array);
